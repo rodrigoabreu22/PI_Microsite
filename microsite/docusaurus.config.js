a559e56fe8acd9870413455ab7df1e361748bd99
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2025-02-19 16:56:03 +0000

    technologies button

--- microsite/docusaurus.config.js
+++ microsite/docusaurus.config.js
@@ -4,7 +4,7 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -86,6 +86,9 @@ const config = {
             label: 'GitHub',
             position: 'right',
           },
+          {
+            to: 'teamPage', label: 'Team', position: 'left'
+          }
         ],
       },
       footer: {
@@ -133,7 +136,7 @@ const config = {
               },
               {
                 label: 'Jorge Domingues',
-                to: 'https://github.com/dashboard'
+                to: 'https://github.com/JorgeGuiDomingues',
               }
             ]
           }
